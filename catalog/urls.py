30775b9c1fad68ae4f8a3c8bb3e2e1d09dbc3529
Mikhail Gubin <mgubin82@gmail.com>
2025-04-26 17:16:18 +0300

    Final changes. Add function feedback_form in views.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, feedback_form
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
+    # path("contacts/", contacts, name="contacts"),
+    path("contacts/", feedback_form, name="feedback_form"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
 
 def home(request):
     """ Контроллер для главной страницы """
@@ -6,5 +8,19 @@ def home(request):
 
 
 def contacts(request):
-    """ Контроллер для страницы Контакты"""
+    """ Контроллер для страницы 'Контакты' """
     return render(request, "contacts.html")
+
+
+def feedback_form(request):
+    """ Контроллер для обработки обратной связи со страницы 'Контакты' """
+
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        # Обработка данных
+        print(f"Получено новое сообщение от {name} ({phone}): \n{message}")
+        return redirect('/')
+    return render(request, 'contacts.html')
