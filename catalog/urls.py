27b104d46b11a77e383bbcf381a53316e586bafa
Mikhail Gubin <mgubin82@gmail.com>
2025-04-26 17:33:35 +0300

    Changed by isort and black.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, feedback_form
+from catalog.views import contacts, feedback_form, home
 
 app_name = CatalogConfig.name
 
