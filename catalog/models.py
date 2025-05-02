4774f55af876507235d0302864fe8ed7ea8b79d4
Mikhail Gubin <mgubin82@gmail.com>
2025-05-02 19:57:43 +0300

    Installed psycopg2-binary, python-dotenv, pillow Changed settings.py. Add models in models.py. Saved first migration

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Наименование категории товаров",
+        help_text="Введите наименование категории товаров",
+    )
+    description = models.TextField(
+        verbose_name="Описание категории товаров",
+        help_text="Введите категории товаров",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "категория товаров"
+        verbose_name_plural = "категории товаров"
+        ordering = ["name"]
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Наименование продукта",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание продукта",
+        help_text="Введите описание продукта",
+        blank=True,
+        null=True,
+    )
+    product_image = models.ImageField(
+        upload_to="product/images",
+        blank=True,
+        null=True,
+        verbose_name="Изображение продукта",
+        help_text="Загрузите изображение продукта",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория продукта",
+        help_text="Введите категорию продукта",
+        blank=True,
+        null=True,
+        related_name="products",
+    )
+    purchase_price = models.FloatField(
+        verbose_name="Стоимость продукта", help_text="Введите стоимость продукта"
+    )
+    created_at = models.DateTimeField(verbose_name="Дата регистрации продукта")
+    updated_at = models.DateTimeField(
+        verbose_name="Дата последнего изменения информации о продукте"
+    )
+
+    def __str__(self):
+        return f"{self.name} из категории {self.category}"
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ["name", "category", "created_at", "updated_at"]
--- config/settings.py
+++ config/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Загружаем переменные окружения из файла .env
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -23,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7*%14610yhyl&6e&(c=ncs99y=q-xubb2ifxu4r%dw-5txt+ks"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('DATABASE_DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -73,10 +76,15 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT', default='5432'),
     }
 }
 
