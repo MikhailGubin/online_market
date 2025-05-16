472cfd7c5ce282dd88e1a7d903394bbe3a1b244d
Mikhail Gubin <mgubin82@gmail.com>
2025-05-16 17:16:42 +0300

    Added urls.py. Added blog_app in settings.py.

---
+++ blog_app/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from blog_app.apps import BlogAppConfig
+from blog_app.views import (contacts, feedback_form, home, ProductDetailView,
+                           ProductListView)
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path("contacts/", feedback_form, name="feedback_form"),
+    path("", ProductListView.as_view(), name="products_list"),
+    path("catalog/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
