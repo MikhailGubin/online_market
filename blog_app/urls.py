2a31a25d2179b2f554cfdd0d38b41f4d8301f4ed
Mikhail Gubin <mgubin82@gmail.com>
2025-05-18 12:00:18 +0300

    Added blog_confirm_delete.html Changed blog_detail.html, blog_list.html, blog_form.html, urls.py, views.py. Renamed my_tags.py to blog_tags.py fpr blog_app

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,15 +3,16 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from blog_app.apps import BlogAppConfig
-from blog_app.views import (BlogDetailView, BlogCreateView,
-                           BlogListView)
+from blog_app.views import (BlogDetailView, BlogListView, BlogCreateView, BlogUpdateView, BlogDeleteView)
 
 app_name = BlogAppConfig.name
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_list"),
     path("<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
-    path("create/", BlogCreateView.as_view(), name="blog_create"),
+    path("new/", BlogCreateView.as_view(), name="blog_create"),
+    path("<int:pk>/edit/", BlogUpdateView.as_view(), name="blog_edit"),
+    path("<int:pk>/delete/", BlogDeleteView.as_view(), name="blog_delete"),
 ]
 
 if settings.DEBUG:
--- blog_app/views.py
+++ blog_app/views.py
@@ -2,22 +2,36 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from blog_app.models import Blog
 
 
 class BlogListView(ListView):
     """ Класс для представления объектов класса 'Product' """
     model = Blog
+    context_object_name = 'blogs'
 
 
 class BlogDetailView(DetailView):
     """ Выводит представление отдельного объекта класса 'Product' """
     model = Blog
-
+    context_object_name = 'blog'
 
 class BlogCreateView(CreateView):
     """ Создаёт представление объекта класса 'Product' """
     model = Blog
     fields = ("title", "content", "preview", "publication_sign", "views_number")
+    success_url = reverse_lazy("blog_app:blog_list")
+
+
+class BlogUpdateView(UpdateView):
+    """ Создаёт представление объекта класса 'Product' """
+    model = Blog
+    fields = ("title", "content", "preview", "publication_sign", "views_number")
+    success_url = reverse_lazy("blog_app:blog_list")
+
+
+class BlogDeleteView(DeleteView):
+    """ Создаёт представление объекта класса 'Product' """
+    model = Blog
     success_url = reverse_lazy("blog_app:blog_list")
