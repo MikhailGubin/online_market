974c645f7fe2530aca5a6cef7486613f3635e72b
Mikhail Gubin <mgubin82@gmail.com>
2025-10-26 18:15:35 +0300

    Changed home.html, README.md, tests.py and views.py

--- catalog/views.py
+++ catalog/views.py
@@ -11,43 +11,6 @@ from catalog.forms import ProductForm, ProductModeratorForm
 from catalog.models import Product
 from catalog.services import ProductService
 
-# def home(request):
-#     """Контроллер для главной страницы"""
-#     return render(request, "home.html")
-
-
-# def contacts(request):
-#     """Контроллер для страницы 'Contacts'"""
-#     return render(request, "contacts.html")
-
-
-# def feedback_form(request):
-#     """Контроллер для обработки обратной связи со страницы 'Contacts'"""
-#
-#     if request.method == "POST":
-#         # Получение данных из формы
-#         name = request.POST.get("name")
-#         phone = request.POST.get("phone")
-#         message = request.POST.get("message")
-#         # Обработка данных
-#         print(f"Получено новое сообщение от {name} ({phone}): \n{message}")
-#         return redirect("/")
-#     return render(request, "contacts.html")
-
-
-# def products_list(request):
-#     """Контроллер для отображения всех продуктов на страницу 'Главная'"""
-#     products = Product.objects.all()
-#     context = {"products": products}
-#     return render(request, "products_list.html", context)
-
-
-# def product_detail(request, pk):
-#     """Контроллер для отображения всей информации по одному продукту на странице 'Информация о товаре'"""
-#     product = get_object_or_404(Product, pk=pk)
-#     context = {"product": product}
-#     return render(request, "product_detail.html", context)
-
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
     """Создаёт представление объекта класса 'Product'"""
